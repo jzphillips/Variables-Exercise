6c90590e07fcaa1f2b10ba2486c315d36cc796e1
Javian Phillips <javianph3@gmail.com>
2025-05-14 18:53:01 -0500

    Created new Program

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bookTitle = "Retrospection of Hampton";
+
+            double bookPrice = 15.99;
+
+            decimal dealPrice = 30.00m;
+
+          
+
+            char tier = 'A';
+
+            int bookRating = 95;
+                
+            Console.WriteLine($"Are you looking for {bookTitle}?");
+            
+            bool favorsBook = true;
+            bool dislikesBook = false;
+            if (favorsBook && !dislikesBook)
+            {
+                Console.WriteLine("That one is recommended a lot.");
+            }
+            Console.WriteLine($"If I had to give it a rating it'd be {bookRating}. Truly an {tier} tier story.");
+            Console.WriteLine($"It goes for {bookPrice}, but you can get two for {dealPrice}.");
         }
     }
 }
